7cf068a5527e4a8af8c1e0aaf82db6ceae5eef2b
QuelbisQuintero <111824468+QuelbisQuintero@users.noreply.github.com>
2022-08-25 18:49:08 -0500

    Initial commit

---
+++ consultorioOnline/src/main/java/com/consultorioSnoopy/consultorioOnline/ConsultorioOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.consultorioSnoopy.consultorioOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioOnlineApplication.class, args);
+	}
+
+}
---
+++ consultorioOnline/src/test/java/com/consultorioSnoopy/consultorioOnline/ConsultorioOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultorioSnoopy.consultorioOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
